8dfd75e6fd4e9ebd9f20b9ff9add38b79d702b40
xiaofei <a871672293@163.com>
2020-09-20 00:19:23 +0800

    vue项目的创建--候鸟

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+   
+    <Telba></Telba>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import Telba from './component/biaoge/index.vue'
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,Telba
+  },
+  
+}
+</script>
+
+<style lang="scss"  >
+	.home .el-table .warning-row {
+	  background: fuchsia;
+	}
+</style>
